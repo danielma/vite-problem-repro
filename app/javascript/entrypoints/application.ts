621fc9251476fc533cd57bb0fe6dbec02e16e902
Daniel Ma <dma@planningcenter.com>
2023-08-29 09:35:45 -0700

    Be a little more explicit about what part is broken

--- app/javascript/entrypoints/application.ts
+++ app/javascript/entrypoints/application.ts
@@ -1,10 +1,13 @@
+import url from '@planningcenter/url'
 import { Client } from '@planningcenter/api-client'
 
 console.log('Vite ⚡️ Rails')
 
 console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
 
+console.log('The url lib itself works', url('development')('people'))
+
 const client = new Client({ app: 'people' })
 
-client.get({ url: 'me', data: { fields: {} } })
-
+console.log("But trying to use it through api-client doesn't")
+client.get({ url: 'me', fields: {} })
