9a55d793db8d24901e869708a9d37131dcbbac03
Daniel Ma <dma@planningcenter.com>
2023-08-29 09:25:29 -0700

    SpaController

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get "spa/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "spa#index"
 end
